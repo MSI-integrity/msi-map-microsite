212ae2ef8ca772ce1a0333678dd5d5011ebac7a0
Ben Isaacs <benjaminisaacs@gmail.com>
2016-12-13 22:44:17 -0800

    fixed bug with data.json

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 - Build Sass and JS with `npm run build`
 - Run server with `npm start` (access at localhost:3000)
 - For development, watch source files with `npm run watch`
-- JSON must be manually generated with `node convert_data.js data/data.xlsx >
-data/data.json`
+- JSON must be manually generated with `node convert_data.js >
+data/data.json`, which reads from data/data.xlsx. Then check in data.json into
+git
--- convert_data.js
+++ convert_data.js
@@ -1,56 +1,48 @@
 const xlsx2json = require('xlsx2json');
 
-var result = undefined;
-
-function convertDataFromXlsx() {
-  return xlsx2json('data/data.xlsx', {
-  	dataStartingRow: 2,
-  	mapping: {
-  		"name": "A",
-	  	"industry": "B",
-      "industry_sub": "C",
-      "mission": "D",
-      "stakeholders": "E",
-      "launched": "F",
-      "human_rights_reference": "G",
-      "involvement_of_affected_communities": "H",
-      "standards": "I",
-      "human_rights_law_reference": "J",
-      "evaluations": "K",
-      "reports": "L",
-      "grievance_mechanisms": "M",
-      "sanctions": "N",
-    }
-  }, function (error, jsonArray) {
-    msis = jsonArray.map(msi => {
-		  m = {};
-		  m.name = msi.name;
-      m.industry = msi.industry;
-      m.industry_sub = msi.industry_sub;
-		  m.mission = msi.mission;
-		  m.stakeholders = msi.stakeholders;
-		  m.launched = msi.launched;
-		  features = [];
-      ["human_rights_reference",
-       "involvement_of_affected_communities",
-       "standards",
-       "human_rights_law_reference",
-       "evaluations",
-       "reports",
-       "grievance_mechanisms",
-       "sanctions"
-      ].forEach(feature => {
-        if (msi[feature] === 'Yes') {
-          features.push(feature);
-        }
-      });
-	    m.features = features;
-      return m;
-	  }); 
-    return new Promise(function (resolve, reject) {
-      resolve(JSON.stringify({data: msis}));
+xlsx2json('data/data.xlsx', {
+  dataStartingRow: 2,
+  mapping: {
+    "name": "A",
+    "industry": "B",
+    "industry_sub": "C",
+    "mission": "D",
+    "stakeholders": "E",
+    "launched": "F",
+    "human_rights_reference": "G",
+    "involvement_of_affected_communities": "H",
+    "standards": "I",
+    "human_rights_law_reference": "J",
+    "evaluations": "K",
+    "reports": "L",
+    "grievance_mechanisms": "M",
+    "sanctions": "N",
+  }
+}, function (error, jsonArray) {
+  msis = jsonArray.pop().map(msi => {
+    m = {};
+    m.name = msi.name;
+    m.industry = msi.industry;
+    m.industry_sub = msi.industry_sub;
+    m.mission = msi.mission;
+    m.stakeholders = msi.stakeholders;
+    m.launched = msi.launched;
+    features = [];
+    ["human_rights_reference",
+     "involvement_of_affected_communities",
+     "standards",
+     "human_rights_law_reference",
+     "evaluations",
+     "reports",
+     "grievance_mechanisms",
+     "sanctions"
+    ].forEach(feature => {
+      if (msi[feature] === 'Yes') {
+        features.push(feature);
+      }
     });
-  });
-}
-
-module.exports = convertDataFromXlsx;
+    m.features = features;
+    return m;
+  }); 
+  console.log({data: msis}); 
+});
