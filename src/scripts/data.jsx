98b2f8805554f75de6d864210ce3c925eb1c32e9
Ben Isaacs <benjaminisaacs@gmail.com>
2017-01-21 12:34:42 -0800

    reformatted data page

--- src/scripts/data.jsx
+++ src/scripts/data.jsx
@@ -69,14 +69,22 @@ var Data = React.createClass({
             <div className="small-12 columns">
               <h1>About The Researchers</h1>
             </div>
-            <div className="small-6 columns">
+            <div className="small-3 columns">
               <img src="images/org_icons/msi-integrity.png" />
+            </div>
+            <div className="small-9 columns">
               <p>The Institute for Multi-Stakeholder Initiative Integrity (“MSI Integrity”) is a non-profit organization dedicated to understanding the human rights impact and value of voluntary initiatives that address business and human rights. MSI Integrity researches key questions surrounding the effectiveness of these initiatives, facilitates learning in the field, and develops tools to evaluate initiatives from a human rights perspective. MSI Integrity takes a particular interest in how initiatives include, empower, and impact affected communities. MSI Integrity was incubated at the International Human Rights Clinic at Harvard Law School from 2010- 2012, and was publicly launched in 2013.</p>
+            </div>
+            <div className="small-3 columns">
               <img src="images/org_icons/miller-chevalier-logo.png" />
+            </div>
+            <div className="small-9 columns">
               <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
             </div>
-            <div className="small-6 columns">
+            <div className="small-3 columns">
               <img src="images/org_icons/dhrc-logo.png" />
+            </div>
+            <div className="small-9 columns">
               <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
             </div>
           </div>
