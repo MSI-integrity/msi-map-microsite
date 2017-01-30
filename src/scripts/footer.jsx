549bef1a54055c5a5be223dfdddd88a86d063e46
Ben Isaacs <benjaminisaacs@gmail.com>
2017-01-29 21:22:00 -0800

    remove copyright

--- src/scripts/footer.jsx
+++ src/scripts/footer.jsx
@@ -2,16 +2,9 @@ var React = require('react');
 
 var Footer = React.createClass({
   render: function () {
-    var photoCopyright;
-    if (this.props.homepage) {
-      photoCopyright = (
-        <p>Image copyrights Moyan Brenn</p>
-      );
-    }
     return (
       <div className="row text-center footer">
         <p>© 2016 <a href="http://www.msi-integrity.org">MSI Integrity</a></p>
-        {photoCopyright}
       </div>
     );
   }
