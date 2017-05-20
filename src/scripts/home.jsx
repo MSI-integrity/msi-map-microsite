68886ce4032df6c6ccc1645fdee87e4d081f8700
Ben Isaacs <benjaminisaacs@gmail.com>
2017-05-19 23:14:05 -0700

    missing hyperlink on home page

--- src/scripts/home.jsx
+++ src/scripts/home.jsx
@@ -12,7 +12,7 @@ var Home = React.createClass({
               <p>Multi-stakeholder initiatives (MSIs) bring together various stakeholders — such as corporations, civil society organizations, governments, and local communities — to collaborate in addressing a specific issue.</p>
               <p>MSIs currently operate in almost every major global industry: certifying the production of clothing and manufactured goods, monitoring commercial fishing practices, establishing codes of conduct for internet companies regarding privacy and freedom of expression, and more. These initiatives have been created to address some of the most intractable problems linked to the global economic system, ranging from forced labor to deforestation and government corruption.</p>
               <p>In collaboration with the law firm Miller & Chevalier, The Institute for Multi-Stakeholder Initiative Integrity (MSI Integrity) and the Duke Human Rights Center at the Kenan Institute for Ethics have produced this online database of transnational standard-setting MSIs. We hope that the database contributes to debate and discussion about the voluntary governance of the private sector and encourages deeper research and critical reflection into the roles, functions and effectiveness of individual MSIs, as well as MSIs as a field.</p>
-              <strong>To learn more, read our summary report (hyperlink to “Report” page), explore <a href="#data">the data</a>, or review our <a href="#questions">FAQ</a></strong>
+              <strong>To learn more, read our <a href="#report">summary report</a>, explore <a href="#data">the data</a>, or review our <a href="#questions">FAQ</a></strong>
             </div>
           </div>
         </div>
